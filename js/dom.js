d4b6ca785e0600e6038508c8327b6495e160be52
nachosala <nachosala89@gmail.com>
2021-08-31 09:23:29 -0300

    Fix linters errors

--- js/dom.js
+++ js/dom.js
@@ -4,16 +4,16 @@ const nav = document.querySelector('nav');
 function switchMenu() {
   nav.classList.toggle('sm-view');
   const image = document.querySelector('img');
-  if (image.getAttribute('src') === "./images/menu.png") {
+  if (image.getAttribute('src') === './images/menu.png') {
     image.setAttribute('src', './images/cancel.png');
   } else {
-    image.setAttribute('src', './images/menu.png')
+    image.setAttribute('src', './images/menu.png');
   }
 }
 
 const menuList = document.querySelectorAll('nav li');
 
 button.addEventListener('click', switchMenu);
-for (let i = 0; i < menuList.length; i++) {
+for (let i = 0; i < menuList.length; i += 1) {
   menuList[i].addEventListener('click', switchMenu);
 }
