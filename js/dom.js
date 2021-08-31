abee6e516397489b0878a5b395839c9d13167c3c
nachosala <nachosala89@gmail.com>
2021-08-31 08:53:26 -0300

    Add dynamic behavior to navbar

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,19 @@
 const button = document.querySelector('#hamburger-icon');
+const nav = document.querySelector('nav');
 
-function displayMenu() {
-  const nav = document.querySelector('nav');
+function switchMenu() {
   nav.classList.toggle('sm-view');
-  const image = document.querySelector('svg');
+  const image = document.querySelector('img');
+  if (image.getAttribute('src') === "./images/menu.png") {
+    image.setAttribute('src', './images/cancel.png');
+  } else {
+    image.setAttribute('src', './images/menu.png')
+  }
 }
 
-button.addEventListener('click', displayMenu);
+const menuList = document.querySelectorAll('nav li');
+
+button.addEventListener('click', switchMenu);
+for (let i = 0; i < menuList.length; i++) {
+  menuList[i].addEventListener('click', switchMenu);
+}
